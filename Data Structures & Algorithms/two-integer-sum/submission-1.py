341bd1e8ccc5ae3883e592c77c4f416f523f63c6
PSKPK <84656575+PSKPK@users.noreply.github.com>
2026-08-16 14:55:01 +0530

    Add: two-integer-sum - submission-1

---
+++ Data Structures & Algorithms/two-integer-sum/submission-1.py
@@ -0,0 +1,24 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        sN = sorted(nums)
+        ri = len(nums)-1
+        li = 0
+        while li < ri:
+            if sN[li] + sN[ri] == target:
+                break
+            if sN[li] + sN[ri] > target:
+                ri = ri-1
+            if sN[li] + sN[ri] < target:
+                li = li + 1
+        v1=sN[li]
+        v2=sN[ri]
+        s1 = None
+        s2 = None
+        for a in range(0, len(nums)):
+            if s1!=None and s2!=None:
+                break
+            if nums[a]==v1 and s1==None:
+                s1 = a
+            if nums[a]==v2 and s2==None:
+                s2 = a
+        return [min(s1, s2), max(s1, s2)]
