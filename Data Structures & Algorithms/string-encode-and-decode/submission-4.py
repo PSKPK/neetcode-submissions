eceeac97fbd22975aa28576ab38db31a2030959d
PSKPK <84656575+PSKPK@users.noreply.github.com>
2026-08-16 15:38:10 +0530

    Add: string-encode-and-decode - submission-4

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-4.py
@@ -0,0 +1,20 @@
+class Solution:
+
+    def encode(self, strs: List[str]) -> str:
+        wholeStr = ""
+        for s in strs:
+            ordS = ""
+            for c in s:
+                ordS = f'{ordS}@{ord(c)}'
+            wholeStr = f'{wholeStr},{ordS}'
+        return wholeStr
+
+    def decode(self, s: str) -> List[str]:
+        if s == "":
+            return []
+        if len(s)==0:
+            return [s]
+        if s[0]== ',':
+            s = s[1:]
+        words = s.split(',')
+        return list(map(lambda x: ''.join(list(map(lambda y: chr(int(y)) if y!='' else '', x.split('@')))), words))
